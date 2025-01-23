e79997d945341c409a93f3b5bb455c386fb30dd8
Olushola <ememobongumoren2@gmail.com>
2025-01-23 22:17:22 +0100

    carousel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,49 @@
+"use client";
+
 import Navbar from "@/components/Navbar";
 import { Col, Row } from "antd";
 import Image from "next/image";
-import firstCarouselImage from "../../public/images/desktop-image-hero-1.jpg"
-import arrowRight from "../../public/images/icon-arrow.svg"
-import carouselRightIcon from "../../public/images/icon-angle-right.svg"
-import carouselLeftIcon from "../../public/images/icon-angle-left.svg"
+import arrowRight from "../../public/images/icon-arrow.svg";
+import carouselRightIcon from "../../public/images/icon-angle-right.svg";
+import carouselLeftIcon from "../../public/images/icon-angle-left.svg";
 import subHero1 from "../../public/images/image-about-dark.jpg";
 import subHero2 from "../../public/images/image-about-light.jpg";
+import { useState } from "react";
 
 export default function Home() {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const handleCarouselPrev = () => {
+    setActiveIndex((prev) => (prev - 1 < 0 ? 0 : prev - 1));
+  };
+
+  const handleCarouselNext = () => {
+    setActiveIndex((prev) =>
+      prev + 1 > shopData.length - 1 ? shopData.length - 1 : prev + 1
+    );
+  };
+
+  const shopData = [
+    {
+      image: "/images/desktop-image-hero-1.jpg",
+      header: "Discover innovative ways to decorate ",
+      description:
+        "We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love.",
+    },
+    {
+      image: "/images/desktop-image-hero-2.jpg",
+      header: "We are available all across the globe ",
+      description:
+        "With stores all over the world, it's easy for you to find furniture for your home or place of business. Locally, we’re in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? Don't hesitate to contact us today. ",
+    },
+    {
+      image: "/images/desktop-image-hero-3.jpg",
+      header: "Manufactured with the best materials",
+      description:
+        "Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office. ",
+    },
+  ];
+
   return (
     <div>
       <Navbar />
@@ -16,18 +51,25 @@ export default function Home() {
         <Row>
           <Col lg={14}>
             <span>
-              <Image src={firstCarouselImage} className="w-full h-full" alt="hero 1" />
+              <Image
+                src={shopData[activeIndex].image}
+                fill
+                className="w-full h-full"
+                alt="hero 1"
+              />
             </span>
           </Col>
           <Col lg={10}>
             <div className="relative flex flex-col justify-center  bg-white w-full h-full ">
               <div className="flex flex-col justify-center text-black md:p-28 items-center">
                 <div>
-                  <h2 className="font-bold text-4xl">Discover innovative ways to decorate</h2>
+                  <h2 className="font-bold text-4xl">
+                    {shopData[activeIndex].header}
+                  </h2>
                   <p className="my-5 text-darkGray">
-                  We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love. 
+                    {shopData[activeIndex].description}
                   </p>
-                  <span className="flex text-xl cursor-pointer hover:translate-x-3 font-semibold items-center gap-5 hover:text-darkGray">
+                  <span className="flex text-xl cursor-pointer hover:translate-x-3 font-semibold items-center gap-5 hover:text-darkGray my-10">
                     <p className="tracking-[0.7em] uppercase">Shop</p>
                     <p className="tracking-[0.7em] uppercase">Now</p>
                     <span>
@@ -37,16 +79,18 @@ export default function Home() {
                 </div>
               </div>
               <div className="flex absolute left-0 bottom-0">
-                <div className="bg-black hover:bg-veryDarkGray hover:cursor-pointer flex justify-between p-8">
-                  <span className="h-full">
-                    <Image src={carouselLeftIcon} alt="carouselLeftIcon" />
-                  </span>
-                </div>
-                <div className="hover:bg-veryDarkGray  hover:cursor-pointer  bg-black flex justify-between p-8">
-                  <span className=" h-full">
-                    <Image src={carouselRightIcon} alt="carouselRightIcon" />
-                  </span>
-                </div>
+                <span
+                  className="bg-black h-full hover:bg-veryDarkGray hover:cursor-pointer flex justify-between p-8"
+                  onClick={handleCarouselPrev}
+                >
+                  <Image src={carouselLeftIcon} alt="carouselLeftIcon" />
+                </span>
+                <span
+                  className="hover:bg-veryDarkGray  hover:cursor-pointer  bg-black flex justify-between p-8 h-full"
+                  onClick={handleCarouselNext}
+                >
+                  <Image src={carouselRightIcon} alt="carouselRightIcon" />
+                </span>
               </div>
             </div>
           </Col>
@@ -54,18 +98,26 @@ export default function Home() {
       </div>
       <div>
         <Row>
-          <Col lg={5} xxl={8}>
+          <Col lg={7} xxl={8}>
             <span>
-              <Image src={subHero1} className="w-full h-full" alt="sub-hero" />
+              <Image fill src={subHero1} className="w-full h-full" alt="sub-hero" />
             </span>
           </Col>
-          <Col lg={12} xxl={8}>
-          <div className="bg-white w-full h-full ">
+          <Col lg={10} xxl={8}>
+            <div className="bg-white w-full h-full ">
               <div className="flex flex-col justify-center text-black md:p-14 items-center">
                 <div>
-                  <h2 className="font-bold uppercase tracking-[0.5em] text-2xl">About our funitures</h2>
+                  <h2 className="font-bold uppercase tracking-[0.5em] text-xl">
+                    About our funitures
+                  </h2>
                   <p className="my-5 text-lg text-darkGray">
-                  Our multifunctional collection blends design and function to suit your individual taste. Make each room unique, or pick a cohesive theme that best express your interests and what inspires you. Find the furniture pieces you need, from traditional to contemporary styles or anything in between. Product specialists are available to help you create your dream space.
+                    About our furniture Our multifunctional collection blends
+                    design and function to suit your individual taste. Make each
+                    room unique, or pick a cohesive theme that best express your
+                    interests and what inspires you. Find the furniture pieces
+                    you need, from traditional to contemporary styles or
+                    anything in between. Product specialists are available to
+                    help you create your dream space.
                   </p>
                 </div>
               </div>
@@ -73,14 +125,11 @@ export default function Home() {
           </Col>
           <Col lg={7} xxl={8}>
             <span>
-              <Image src={subHero2} className="w-full h-full"  alt="sub-hero" />
+              <Image fill src={subHero2} className="w-full h-full" alt="sub-hero" />
             </span>
           </Col>
-
         </Row>
       </div>
-
     </div>
-      
   );
 }
